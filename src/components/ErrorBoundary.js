3b7869421455b0bf46f04ef4da54cbb9315c8cc0
jennydunix <jennifer.o.odunze@gmail.com>
2022-11-05 08:41:47 +0100

    Added a button for navigation

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import informed from "../informed.svg"
+import informed from "../informed.svg";
 
 export class ErrorBoundary extends Component {
   constructor(props) {
@@ -20,10 +20,31 @@ export class ErrorBoundary extends Component {
   }
   render() {
     if (this.state.error)
-      return <div style={{ color: "red" }}>
-        <h2 style={{marginTop:"30px"}}>{this.props.msg || "Something went wrong"}</h2>
-        <img src={informed} alt="informed" width="400px" style={{marginTop:"70px"}}/>
-      </div>;
+      return (
+        <div style={{ color: "red" }}>
+          <h2 style={{ marginTop: "30px" }}>
+            {this.props.msg || "Something went wrong"}
+          </h2>
+          <img
+            src={informed}
+            alt="informed"
+            width="400px"
+            style={{ marginTop: "70px" }}
+          />
+          <br />
+          <button
+            style={{
+              padding: "15px 30px",
+              border: "1px solid black",
+              backgroundColor: "white",
+              marginTop: "30px",
+              borderRadius: "50px"
+            }}
+          >
+            <a href="/dashboard">Go back</a>
+          </button>
+        </div>
+      );
 
     return this.props.children;
   }
